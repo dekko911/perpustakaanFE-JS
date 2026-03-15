7711987484b3a3acf91ad7e672b8afd320fe5e12
dekko911 <popokadal3@gmail.com>
2026-03-15 19:17:00 +0800

    added pagination

--- src/components/ui/table.jsx
+++ src/components/ui/table.jsx
@@ -1,5 +1,5 @@
 import { AiFillDelete } from "react-icons/ai";
-import { FaEdit } from "react-icons/fa";
+import { FaChevronLeft, FaChevronRight, FaEdit } from "react-icons/fa";
 import { Link, useLocation } from "react-router";
 import { cn } from "../../lib/utils";
 
@@ -51,20 +51,23 @@ export const TBodyCell = ({ children, className = "" }) => {
 
 // table cell body <td>, but this for action column
 export const TBodyCellAction = ({ to, onClick, className = "" }) => {
-	const location = useLocation()
+	const location = useLocation();
 
 	return (
 		<td
 			className={cn(
 				"py-7 border-b border-zinc-500/30 grid grid-cols-2 items-center justify-center",
-				className
+				className,
 			)}
 		>
-			{location.pathname !== "/roles" &&
+			{location.pathname !== "/roles" && (
 				<Link to={to} className="w-0">
-					<FaEdit size={33} className="hover:text-green-500 hover:duration-200" />
+					<FaEdit
+						size={33}
+						className="hover:text-green-500 hover:duration-200"
+					/>
 				</Link>
-			}
+			)}
 			<button onClick={onClick} className="cursor-pointer w-0">
 				<AiFillDelete
 					size={33}
@@ -72,5 +75,89 @@ export const TBodyCellAction = ({ to, onClick, className = "" }) => {
 				/>
 			</button>
 		</td>
+	);
+};
+
+export const TablePagination = ({
+	handlePrev,
+	handleNext,
+	pageIndex,
+	setPageIndex,
+	lastPage,
+	totalData,
+}) => {
+	return (
+		<div className="flex items-center justify-between border-t border-white/10 px-4 py-3 sm:px-6">
+			<div className="flex flex-1 justify-between sm:hidden">
+				<button
+					onClick={handlePrev}
+					disabled={pageIndex === 1}
+					className="relative inline-flex items-center rounded-md border border-white/10 bg-white/5 px-4 py-2 text-sm font-medium text-gray-200 hover:bg-white/10"
+				>
+					Previous
+				</button>
+
+				<button
+					onClick={handleNext}
+					disabled={pageIndex === lastPage}
+					className="relative ml-3 inline-flex items-center rounded-md border border-white/10 bg-white/5 px-4 py-2 text-sm font-medium text-gray-200 hover:bg-white/10"
+				>
+					Next
+				</button>
+			</div>
+			<div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
+				<div>
+					<p className="text-sm text-gray-300">
+						Showing <span className="font-medium">{pageIndex}</span> to{" "}
+						<span className="font-medium">{lastPage}</span> of{" "}
+						<span className="font-medium">{totalData}</span> results
+					</p>
+				</div>
+				<div>
+					<nav
+						aria-label="Pagination"
+						className="isolate inline-flex -space-x-px rounded-md"
+					>
+						<button
+							onClick={handlePrev}
+							disabled={pageIndex === 1}
+							className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 inset-ring inset-ring-gray-700 hover:bg-white/5 focus:z-20 focus:outline-offset-0 cursor-pointer"
+						>
+							<span className="sr-only">Previous</span>
+							<FaChevronLeft aria-hidden="true" className="size-5" />
+						</button>
+
+						{Array.from({ length: lastPage }).map((_, i) => {
+							const fixPage = i + 1;
+
+							return (
+								<button
+									key={fixPage}
+									onClick={() => setPageIndex(fixPage)}
+									aria-current="page"
+									className={cn(
+										"cursor-pointer",
+										pageIndex === fixPage
+											? "relative z-10 inline-flex items-center px-4 py-2 text-sm font-semibold focus:z-20 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500 cursor-pointer bg-indigo-500 text-white"
+											: "relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-200 inset-ring inset-ring-gray-700 hover:bg-white/5 focus:z-20 focus:outline-offset-0",
+									)}
+								>
+									{fixPage}
+								</button>
+							);
+						})}
+
+						<button
+							onClick={handleNext}
+							className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 inset-ring inset-ring-gray-700 hover:bg-white/5 focus:z-20 focus:outline-offset-0 cursor-pointer"
+							disabled={pageIndex === lastPage}
+						>
+							<span className="sr-only">Next</span>
+							<FaChevronRight aria-hidden="true" className="size-5" />
+						</button>
+					</nav>
+				</div>
+			</div>
+		</div>
 	);
 };
--- src/pages/users/index.jsx
+++ src/pages/users/index.jsx
@@ -3,13 +3,14 @@ import { FaPlus } from "react-icons/fa";
 import { Link } from "react-router";
 import { InputSearch } from "../../components/ui/input";
 import {
-  Table,
-  TableBody,
-  TableHead,
-  TableRow,
-  TBodyCell,
-  TBodyCellAction,
-  THeadCell,
+	Table,
+	TableBody,
+	TableHead,
+	TablePagination,
+	TableRow,
+	TBodyCell,
+	TBodyCellAction,
+	THeadCell,
 } from "../../components/ui/table";
 import { AuthLayout } from "../../layouts/auth/index";
 import { swalDialogConfirm, swalToast } from "../../lib/sweet-alert";
@@ -17,133 +18,154 @@ import { axios_api_init } from "../../lib/utils";
 import Loading from "/src/assets/images/pyramid-19507.gif";
 
 export const UsersPage = () => {
-  const [users, setUsers] = useState([]);
-  const [isSearch, setIsSearch] = useState("");
-  const [isLoading, setIsLoading] = useState(true);
+	const [users, setUsers] = useState([]);
+	const [lastPage, setLastPage] = useState(1);
+	const [total, setTotal] = useState(null);
+	const [isSearch, setIsSearch] = useState("");
+	const [isLoading, setIsLoading] = useState(true);
+	const [pageIndex, setPageIndex] = useState(1);
 
-  useEffect(() => {
-    const fetchUsers = async () => {
-      const res = await axios_api_init.get(`/api/users`, {
-        method: "GET",
-      });
+	if (pageIndex < 1) setPageIndex(1);
 
-      setUsers(res.data.data);
-      setIsLoading(false);
-    };
+	const handleNext = () => {
+		setPageIndex((prev) => prev + 1);
+	};
 
-    const fetchData = async () => {
-      const res = await axios_api_init.get(
-        `/search/users?search_u=${isSearch}`,
-        {
-          method: "GET",
-        },
-      );
+	const handlePrev = () => {
+		setPageIndex((prev) => Math.max(prev - 1, 1));
+	};
 
-      setUsers(res.data.data.hits);
-    };
+	useEffect(() => {
+		const fetchUsers = async () => {
+			const res = await axios_api_init.get(`/api/users?page=${pageIndex}`, {
+				method: "GET",
+			});
 
-    fetchUsers();
+			setTotal(res.data.total_data);
+			setLastPage(res.data.last_page);
+			setUsers(res.data.data);
+			setIsLoading(false);
+		};
 
-    if (isSearch) {
-      fetchData();
-    }
-    //  else if (isSearch === "") {
-    //   fetchUsers();
-    // }
-  }, [isSearch]);
+		const fetchData = async () => {
+			const res = await axios_api_init.get(
+				`/search/users?search_u=${isSearch}`,
+				{
+					method: "GET",
+				},
+			);
 
-  const handleDelete = (id) => {
-    swalDialogConfirm(
-      "Deleting User",
-      "Are You Sure Delete This User?",
-      "info",
-    ).then(async (equals) => {
-      try {
-        if (equals.isConfirmed) {
-          const res = await axios_api_init.delete(`/api/user/${id}`, {
-            method: "DELETE",
-          });
+			setUsers(res.data.data.hits);
+		};
 
-          if (res.data) {
-            swalToast("success", "Successful Deleted!", 280);
-            setUsers((prev) => prev.filter((user) => user.id !== id));
-          }
-        }
-      } catch (err) {
-        // console.error(err);
-        if (err.response.data.code === 403) {
-          swalToast("error", `${err.response.data.error}`, 305);
-        }
-      }
-    });
-  };
+		fetchUsers();
 
-  return (
-    <AuthLayout>
-      <div className="flex flex-col relative">
-        {isLoading && (
-          <img
-            src={Loading}
-            alt="loading..."
-            className="absolute w-100 top-28 left-90"
-          />
-        )}
+		if (isSearch) {
+			fetchData();
+		}
+	}, [isSearch, pageIndex]);
 
-        <div className="flex">
-          <h1 className="font-semibold text-2xl me-0.5">Users Page</h1>
-          <Link to="/users/create" className="w-0">
-            <FaPlus className="hover:text-amber-600 cursor-pointer hover:duration-200" />
-          </Link>
+	const handleDelete = (id) => {
+		swalDialogConfirm(
+			"Deleting User",
+			"Are You Sure Delete This User?",
+			"info",
+		).then(async (equals) => {
+			try {
+				if (equals.isConfirmed) {
+					const res = await axios_api_init.delete(`/api/user/${id}`, {
+						method: "DELETE",
+					});
 
-          <InputSearch
-            onKeyUp={(e) => {
-              setIsSearch(e.target.value);
-            }}
-            placeholder="Search User . . ."
-            className="ms-auto"
-          />
-        </div>
+					if (res.data) {
+						swalToast("success", "Successful Deleted!", 280);
+						setUsers((prev) => prev.filter((user) => user.id !== id));
+					}
+				}
+			} catch (err) {
+				// console.error(err);
+				if (err.response.data.code === 403) {
+					swalToast("error", `${err.response.data.error}`, 305);
+				}
+			}
+		});
+	};
 
-        <Table className="m-0 mt-6">
-          <TableHead>
-            <TableRow>
-              <THeadCell>No.</THeadCell>
-              <THeadCell>Name</THeadCell>
-              <THeadCell>Email</THeadCell>
-              <THeadCell>Roles</THeadCell>
-              <THeadCell>Avatar</THeadCell>
-              <THeadCell className="border-none">Action</THeadCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {users.map((user, i) => (
-              <TableRow key={i}>
-                <TBodyCell>{i + 1}</TBodyCell>
-                <TBodyCell>{user.name}</TBodyCell>
-                <TBodyCell>{user.email}</TBodyCell>
-                <TBodyCell>
-                  {user.roles !== null
-                    ? user.roles?.map((role) => role.name).join(", ")
-                    : "-"}
-                </TBodyCell>
-                <TBodyCell>
-                  <img
-                    src={`${user.r2_avatar_url}`}
-                    alt="avatar"
-                    className="size-18 rounded-[4cap] object-cover object-center mx-auto"
-                  />
-                </TBodyCell>
-                <TBodyCellAction
-                  to={`/users/${user.id}`}
-                  onClick={() => {
-                    handleDelete(user.id);
-                  }}
-                />
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </div>
-    </AuthLayout>
-  );
+	return (
+		<AuthLayout>
+			<div className="flex flex-col relative">
+				{isLoading && (
+					<img
+						src={Loading}
+						alt="loading..."
+						className="absolute w-100 top-28 left-90"
+					/>
+				)}
+
+				<div className="flex">
+					<h1 className="font-semibold text-2xl me-0.5">Users Page</h1>
+					<Link to="/users/create" className="w-0">
+						<FaPlus className="hover:text-amber-600 cursor-pointer hover:duration-200" />
+					</Link>
+
+					<InputSearch
+						onKeyUp={(e) => {
+							setIsSearch(e.target.value);
+						}}
+						placeholder="Search User . . ."
+						className="ms-auto"
+					/>
+				</div>
+
+				<Table className="m-0 mt-6">
+					<TableHead>
+						<TableRow>
+							<THeadCell>No.</THeadCell>
+							<THeadCell>Name</THeadCell>
+							<THeadCell>Email</THeadCell>
+							<THeadCell>Roles</THeadCell>
+							<THeadCell>Avatar</THeadCell>
+							<THeadCell className="border-none">Action</THeadCell>
+						</TableRow>
+					</TableHead>
+					<TableBody>
+						{users.map((user, i) => (
+							<TableRow key={i}>
+								<TBodyCell>{(pageIndex - 1) * 10 + i + 1}</TBodyCell>
+								<TBodyCell>{user.name}</TBodyCell>
+								<TBodyCell>{user.email}</TBodyCell>
+								<TBodyCell>
+									{user.roles !== null
+										? user.roles?.map((role) => role.name).join(", ")
+										: "-"}
+								</TBodyCell>
+								<TBodyCell>
+									<img
+										src={`${user.r2_avatar_url}`}
+										alt="avatar"
+										className="size-18 rounded-[4cap] object-cover object-center mx-auto"
+									/>
+								</TBodyCell>
+								<TBodyCellAction
+									to={`/users/${user.id}`}
+									onClick={() => {
+										handleDelete(user.id);
+									}}
+								/>
+							</TableRow>
+						))}
+					</TableBody>
+				</Table>
+
+				<TablePagination
+					handlePrev={handlePrev}
+					handleNext={handleNext}
+					pageIndex={pageIndex}
+					setPageIndex={setPageIndex}
+					lastPage={lastPage}
+					totalData={total}
+				/>
+			</div>
+		</AuthLayout>
+	);
 };
